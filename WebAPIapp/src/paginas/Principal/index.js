f8bf9a31db24db01d390e5975816aa06a264e582
Guilherme Livianu <guilherme.livianu@gmail.com>
2024-04-02 23:03:50 -0300

    Buscando repositórios

--- WebAPIapp/src/paginas/Principal/index.js
+++ WebAPIapp/src/paginas/Principal/index.js
@@ -59,7 +59,9 @@ export default function Principal({ navigation }) {
               </View>
             </View>
             <TouchableOpacity
-              onPress={() => navigation.navigate("Repositorios")}
+              onPress={() =>
+                navigation.navigate("Repositorios", { id: usuario.id })
+              }
             >
               <Text style={estilos.repositorios}>Ver os repositórios</Text>
             </TouchableOpacity>
